a23b9deb77af17dc84c9d6d884ce38f7f3cb0420
DuyLeVu <levuduy132@gmail.com>
2021-09-14 23:21:14 +0700

    Add good-fast-cheap

--- doyouloveme/js/script.js
+++ doyouloveme/js/script.js
@@ -2,8 +2,8 @@ function repYes(){
     alert('<3');
 }
 function repNO(){
-    x = Math.round(Math.random() * window.innerWidth);
-    y = Math.round(Math.random() * window.innerHeight);
+    x = Math.floor(Math.random() * window.innerWidth);
+    y = Math.floor(Math.random() * window.innerHeight);
     document.getElementById('sayNo').style.top = y + 'px';
     document.getElementById('sayNo').style.left = x + 'px';
 }
--- good_fast_cheap/js/script.js
+++ good_fast_cheap/js/script.js
@@ -1,3 +1,27 @@
-
-
+var isGood = document.getElementById('good');
+var isCheap = document.getElementById('cheap');
+var isFast = document.getElementById('fast');
+function checkGood() {
+    if (isGood.checked) {
+            if (isFast.checked && isCheap.checked) {
+                isFast.checked = false;
+            }
+        } else isGood.checked;
 }
+function checkCheap() {
+    if (isCheap.checked) {
+        if (isFast.checked && isGood.checked) {
+            isGood.checked = false;
+        }
+        else isCheap.checked
+    }
+}
+function checkFast() {
+    if (isFast.checked) {
+        if (isCheap.checked && isGood.checked) {
+            isCheap.checked = false;
+        }
+        else isFast.checked;
+    }
+}
+
